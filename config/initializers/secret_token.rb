0436a58077ca299acc01fcc02d83e9e03c8051ac
vinestreet <mark@vinestreetinteractive.com>
2011-03-08 23:25:40 -0500

    Innitial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cashflow::Application.config.secret_token = '0fb632aa8dec2a43b6729abfba915d03e959f412c3aa22a02c03190f36876a81820014ff20e3e187a40af4c73c41972a63fe23f3e794f90de80c817ce56dc419'
